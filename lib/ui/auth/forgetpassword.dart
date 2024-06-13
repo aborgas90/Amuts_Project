da4201cf1c8c91d2b1c05fbc6a768ad54583dd67
aborgas90 <wildanmuh7@gmail.com>
2024-06-13 15:53:52 +0700

    add: feature forget password

--- lib/ui/auth/forgetpassword.dart
+++ lib/ui/auth/forgetpassword.dart
@@ -1,9 +1,9 @@
-import 'package:amuts_project/utils/routes.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:amuts_project/ui/auth/firebaseexception.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class forgetPasswordScreen extends StatefulWidget {
+  static const String id = 'reset_password';
   const forgetPasswordScreen({Key? key}) : super(key: key);
 
   @override
@@ -11,60 +11,184 @@ class forgetPasswordScreen extends StatefulWidget {
 }
 
 class _forgetPasswordScreenState extends State<forgetPasswordScreen> {
+  final _key = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  static final auth = FirebaseAuth.instance;
+  static late AuthStatus _status;
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    super.dispose();
+  }
+
+  Future<AuthStatus> resetPassword({required String email}) async {
+    await auth
+        .sendPasswordResetEmail(email: email)
+        .then((value) => _status = AuthStatus.successful)
+        .catchError(
+            (e) => _status = AuthExceptionHandler.handleAuthException(e));
+
+    return _status;
+  }
+
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            onPressed: () {
-              Navigator.pushNamed(context, rLogin);
-            },
-            icon: Icon(Icons.arrow_back),
-          ),
-          backgroundColor: Colors.white,
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back),
         ),
-        body: Container(
-          margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 3),
-          child: ListView(
-            children: [
-              const Text(
-                "Forget Password",
-                style: TextStyle(fontSize: 28),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const Text(
-                "Email",
-                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  prefixIcon: Icon(Icons.email_outlined),
-                  border: UnderlineInputBorder(),
-                  labelText: "",
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color.fromRGBO(0, 185, 139, 1),
-                ),
-                onPressed: () {
-                  Navigator.pushNamed(context, rverificationOTP);
-                },
-                child: const Text(
-                  "Send",
+        backgroundColor: Colors.white,
+      ),
+      body: Container(
+        width: size.width,
+        height: size.height,
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+          child: Form(
+            key: _key,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "Forgot Password",
                   style: TextStyle(
-                      fontSize: 17,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white),
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
                 ),
-              )
-            ],
+                const SizedBox(height: 10),
+                const Text(
+                  'Please enter your email address to recover your password.',
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(height: 40),
+                const Text(
+                  'Email address',
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Container(
+                  child: TextFormField(
+                    obscureText: false,
+                    controller: _emailController,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Empty email';
+                      }
+                      return null;
+                    },
+                    autofocus: false,
+                    style: const TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey),
+                    decoration: const InputDecoration(
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                      border: OutlineInputBorder(
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(30.0))),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.black,
+                          width: 1,
+                        ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(
+                            30.0,
+                          ),
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(
+                            30.0,
+                          ),
+                        ),
+                      ),
+
+                      isDense: true,
+                      // fillColor: kPrimaryColor,
+                      filled: true,
+                      errorStyle: TextStyle(fontSize: 15),
+                      hintText: 'email address',
+                      hintStyle: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                const Expanded(child: SizedBox()),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height / 20,
+                  child: Material(
+                    elevation: 2,
+                    borderRadius: BorderRadius.circular(20),
+                    color: Color.fromRGBO(0, 185, 139, 1),
+                    child: MaterialButton(
+                      onPressed: () async {
+                        if (_key.currentState!.validate()) {
+                          final _status = await resetPassword(
+                              email: _emailController.text.trim());
+                          if (_status == AuthStatus.successful) {
+                            //your logic
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(
+                                    'Password has been successfully reset.'),
+                                duration: Duration(
+                                    seconds: 3), // Durasi SnackBar ditampilkan
+                              ),
+                            );
+                          } else {
+                            // Show SnackBar with error message
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(
+                                    'Failed to reset password. Please try again.'),
+                                duration: Duration(
+                                    seconds: 3), // Durasi SnackBar ditampilkan
+                              ),
+                            );
+                          }
+                        }
+                      },
+                      minWidth: double.infinity,
+                      child: const Text(
+                        'SEND',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                            fontFamily: 'Poppins'),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+              ],
+            ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
